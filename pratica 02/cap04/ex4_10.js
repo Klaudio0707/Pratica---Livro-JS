4e8d0b2e94ccbc95ec65e89d5578c3c8c7ac7d5d
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-05-09 20:37:09 +0000

    Novas Atividades

---
+++ pratica 02/cap04/ex4_10.js
@@ -0,0 +1,12 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#resp1");
+const resp2 = document.querySelector("#resp2");
+
+frm.addEventListener("submit", (e) => {
+const valorA = Number(frm.inLadoA.value);
+const valorB = Number(frm.inLadoB.value);
+const valorC = Number(frm.inLadoC.value);
+
+
+
+});
--- pratica 02/cap04/ex4_9.js
+++ pratica 02/cap04/ex4_9.js
@@ -1,25 +1,29 @@
 const frm = document.querySelector("form");
-const resp = document.querySelector("h3");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
 
 frm.addEventListener("submit", (e) => {
 e.preventDefault();
 const valor = Number(frm.inValue.value);
 
 if(valor < 1){
-alert("Valor Insuficiente");
+resp1.innerText = `Valor R$ ${valor.toFixed(2)} Insuficiente`;
 frm.inValue.focus();
 return;
 }
 if (valor >= 1 ) {
- resp.innerText = ` Tempo: 60`;   
+    let resto = valor % 1.00;
+    resp1.innerText = `Tempo: 30min`
+    resp2.innerText = `Troco R$: ${resto.toFixed(2)}`;   
+} if (valor >= 1.75) {
+    let resto = valor % 1.75;
+    resp1.innerText = `Tempo: 60min`
+    resp2.innerText = `Troco R$: ${resto.toFixed(2)}`;
+} if (valor >= 3.00){
+    let resto = valor % 3.00;
+    resp1.innerText = `Tempo: 120min`
+    resp2.innerText = `Troco R$: ${resto.toFixed(2)}`;
 }
-
-
-
-
-
-
-
 
 
 });
